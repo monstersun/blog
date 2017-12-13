16da434515d0b2e0eab60924a1201983d7a1edf8
Teron <651745272@qq.com>
2017-12-13 17:11:26 +0800

    12.13xiaban

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from app.model import User, Role
+from app.model import User, Role, Post
 from flask_migrate import MigrateCommand, Migrate
 from flask_script import Manager, Shell
 
@@ -10,7 +10,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db,User=User,Role=Role)
+    return dict(app=app, db=db,User=User,Role=Role, Post=Post)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
